2a372fcf52a0e596c4e74be027dbb1837ddd0631
Brandt <brandt.strait@gmail.com>
2018-04-10 23:54:42 -0400

    server update, passport auth partially working, new files for login on html

--- public/js/pokemon.js
+++ public/js/pokemon.js
@@ -1,79 +1,49 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDJ8sRVZ5WBZ6RK8tT__eZ81ZMJfsilONM",
-    authDomain: "pokemon-cf1a6.firebaseapp.com",
-    databaseURL: "https://pokemon-cf1a6.firebaseio.com",
-    projectId: "pokemon-cf1a6",
-    storageBucket: "pokemon-cf1a6.appspot.com",
-    messagingSenderId: "645564358111"
-};
-firebase.initializeApp(config);
 
-var userDatabase = firebase.database();
-
-var userEmail = $('#userEmail'); 
-console.log(userEmail);
-var userPassword = $('#userPassword');
-var btnLogIn = $('#btnLogIn');
-var btnSignUp = $('#btnSignUp');
-var btnLogOut = $('#btnLogOut');
+// var userEmail = $('#userEmail'); 
+// console.log(userEmail);
+// var userPassword = $('#userPassword');
+// var btnLogIn = $('#btnLogIn');
+// var btnSignUp = $('#btnSignUp');
+// var btnLogOut = $('#btnLogOut');
 
 
-// sign up
-btnSignUp.on('click', function (event) {
-    event.preventDefault();
-    // Get email and pass  check for real email
-    var email = userEmail.val();
-    var password = userPassword.val();
-    var auth = firebase.auth();
+// // sign up
+// btnSignUp.on('click', function (event) {
+//     event.preventDefault();
+//     // Get email and pass  check for real email
+//     var email = userEmail.val();
+//     var password = userPassword.val();
+   
 
-    // sign up
-    auth.createUserWithEmailAndPassword(email, password).catch(function (err) {
-        console.log(err.message)
-    })
-});
+//     // sign up
+//     auth.createUserWithEmailAndPassword(email, password).catch(function (err) {
+//         console.log(err.message)
+//     })
+// });
 
-// Log In Event
-btnLogIn.on('click', function (event) {
-    event.preventDefault();
-    // Get email and pass
-    var email = userEmail.val();
-    var password = userPassword.val();
-    var auth = firebase.auth();
+// // Log In Event
+// btnLogIn.on('click', function (event) {
+//     event.preventDefault();
+//     // Get email and pass
+//     var email = userEmail.val();
+//     var password = userPassword.val();
+ 
 
-    // sign in
-    auth.signInWithEmailAndPassword(email, password).catch(function (err) {
-        console.log(err.message)
-    })
+//     // sign in
+//     auth.signInWithEmailAndPassword(email, password).catch(function (err) {
+//         console.log(err.message)
+//     })
+// });
 
-});
-
-btnLogOut.on('click', function (event) {
-    event.preventDefault();
-    firebase.auth().signOut().then(function () {
-        // Sign-out successful.
-    }).catch(function (error) {
-        // An error happened.
-    });
-})
-
-
-
-firebase.auth().onAuthStateChanged(function (firebaseUser) {
-    if (firebaseUser) {
-        console.log(firebaseUser);
-    }
-    else {
-        console.log('not logged in');
-    }
-});
-
+// btnLogOut.on('click', function (event) {
+//     event.preventDefault();
+  
+// })
 
 $('.pokemon').on('click', function() {
     event.preventDefault();
     
     $('#pokeModal').modal('toggle');
-
 
 })
 
