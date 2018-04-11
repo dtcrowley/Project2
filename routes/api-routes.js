a7509c5e3fbb410e5598b5395d5b92a027cfbf45
Connor Reed <connorreed@wifi_guest-10-38-98-27.uncc.edu>
2018-04-11 14:50:41 -0400

    search by name

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,49 +1,65 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     db.pokemonstats.findAll({
       include: [db.images]
-    }).then(function(result) {
-      res.render('index', {pokemon: result} )
+    }).then(function (result) {
+      res.render('index', { pokemon: result })
     });
   });
+
+
   
-  
-  app.get('/api/pokemon/image', function(req,res) {
-    db.images.findAll({}).then(function(result) {
+  app.get("/:pokeName", function (req, res) {
+    
+    db.pokemonstats.findAll({
+      where: {
+        pokeName: req.params.pokeName,
+        
+      },
+      include: [db.images]
+    }).then(function (result) {
+      res.render('index', { pokemon: result });
+    })
+  });
+
+
+
+  app.get('/api/pokemon/image', function (req, res) {
+    db.images.findAll({}).then(function (result) {
       res.json(result)
     })
   });
 
-// Display data for all pokemon
-  app.get("/api/pokemon/", function(req, res) {
+  // Display data for all pokemon
+  app.get("/api/pokemon/", function (req, res) {
     db.pokemonstats.findAll()
-      .then(function(PokeDb) {
+      .then(function (PokeDb) {
         res.json(PokeDb);
       });
   });
 
-// Display data for all pokemon of a certain type
-  app.get("/api/pokemon/:Type_1", function(req, res) {
+  // Display data for all pokemon of a certain type
+  app.get("/api/pokemon/:Type_1", function (req, res) {
     db.pokemonstats.findAll({
       where: {
         Type_1: req.params.Type_1
       }
-    }).then(function(PokeDb){
+    }).then(function (PokeDb) {
       res.json(PokeDb);
     })
   });
 
-// Display pokemon data for a given name
-  app.get("/api/:pokeName", function(req, res){
+  // Display pokemon data for a given name
+  app.get("/api/:pokeName", function (req, res) {
     db.pokemonstats.findAll({
       where: {
         pokeName: req.params.pokeName
       },
       include: [db.images]
-    }).then(function(PokeDb){
+    }).then(function (PokeDb) {
       res.json(PokeDb);
     })
   });
@@ -58,12 +74,12 @@ module.exports = function(app) {
   //   })
   // });
 
-  
+
   // app.delete("/api/", function(req, res) {
 
   // });
 
-  
+
   // app.put("/api/:anything", function(req, res) {
 
   // });
