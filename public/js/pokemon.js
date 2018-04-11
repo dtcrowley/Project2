df9af60d184b2d832cc9063364fdccb85375ba1b
Connor Reed <connorreed@wifi_guest-10-38-98-27.uncc.edu>
2018-04-11 11:42:03 -0400

    Have modals popping up on second click.  Working on bug for blank on first click

--- public/js/pokemon.js
+++ public/js/pokemon.js
@@ -1,98 +1,21 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyAlny4lMf5WEg_jYCZME2HHP1gbkzuP0KQ",
-    authDomain: "pokedex-user-auth.firebaseapp.com",
-    databaseURL: "https://pokedex-user-auth.firebaseio.com",
-    projectId: "pokedex-user-auth",
-    storageBucket: "pokedex-user-auth.appspot.com",
-    messagingSenderId: "38189101021"
-};
-firebase.initializeApp(config);
-
-var userDatabase = firebase.database();
-
-var userEmail = $('#userEmail');
-console.log(userEmail);
-var userPassword = $('#userPassword');
-var btnLogIn = $('#btnLogIn');
-var btnSignUp = $('#btnSignUp');
-var btnLogOut = $('#btnLogOut');
-
-
-// sign up
-btnSignUp.on('click', function (event) {
-    event.preventDefault();
-    // Get email and pass  check for real email
-    var email = userEmail.val();
-    var password = userPassword.val();
-    var auth = firebase.auth();
-
-    // sign up
-    auth.createUserWithEmailAndPassword(email, password).catch(function (err) {
-        console.log(err.message)
-    })
-});
-
-// Log In Event
-btnLogIn.on('click', function (event) {
-    event.preventDefault();
-    // Get email and pass
-    var email = userEmail.val();
-    var password = userPassword.val();
-    var auth = firebase.auth();
-
-    // sign in
-    auth.signInWithEmailAndPassword(email, password).catch(function (err) {
-        console.log(err.message)
-    })
-
-});
-
-btnLogOut.on('click', function (event) {
-    event.preventDefault();
-    firebase.auth().signOut().then(function () {
-        // Sign-out successful.
-    }).catch(function (error) {
-        // An error happened.
-    });
-})
-
-
-
-firebase.auth().onAuthStateChanged(function (firebaseUser) {
-    if (firebaseUser) {
-        console.log(firebaseUser);
-    }
-    else {
-        console.log('not logged in');
-    }
-});
-
-// $('.pokemon').on('click', function(event) {
-// if($(this)) {
-//     var passedName = $(this).attr('data-id');
-//     console.log(passedName);
-//     }
-// });
-
-
+// Code for modal popups on a specific pokemon being clicked //////////////////////////////////////
 $('#pokeModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget);
     var base = location.origin;
-    
+
     $('.pokemon').on('click', function (event) {
         if ($(this)) {
             var passedName = $(this).attr('data-id');
             console.log(passedName);
         }
-       
+
         var res = button.data($.ajax({
             url: base + '/api/' + passedName, success: function (result) {
                 console.log(result[0]);
                 $(".modal-title").html(result[0].pokeName);
                 $(".modal-body").html('<br> \nAttack: ' + result[0].Attack + '<br> \nDefense: ' + result[0].Defense +
-                '<br> \nHP: ' + result[0].HP + '<br> \nSpeed: ' + result[0].Speed + '<br> \nSpecial Attack: ' + 
-                result[0].Special_atk + '<br> \nSpecial Defense: ' + result[0].Special_def)
+                    '<br> \nHP: ' + result[0].HP + '<br> \nSpeed: ' + result[0].Speed + '<br> \nSpecial Attack: ' +
+                    result[0].Special_atk + '<br> \nSpecial Defense: ' + result[0].Special_def)
                 $('.pokeI').attr('src', '.' + result[0].images[0].img)
                 //  var modal = $(this)
                 //  console.log(modal);
@@ -101,7 +24,6 @@ $('#pokeModal').on('show.bs.modal', function (event) {
         }))
     });
 });
-
 $(document).ready(function () {
 
     $('.pokemon').on('click', function (event) {
@@ -111,6 +33,7 @@ $(document).ready(function () {
     });
 
 })
+//////////////////////////////////////////////////////////////////////////////////
 
 
 
