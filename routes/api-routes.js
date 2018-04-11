9548ce1a9ce67bb8fa71cd398eaafb9de4970915
Brandt <brandt.strait@gmail.com>
2018-04-11 15:23:08 -0400

    login filepaths working, not yet tracking user login

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 var db = require("../models");
-var passport = require("../config/passport")
+var passport = require("../config/passport");
+
 module.exports = function (app) {
 
   app.get("/", function(req, res) {
@@ -10,52 +11,6 @@ module.exports = function (app) {
     });
   });
 
-  app.post("/api/login", passport.authenticate("local"), function(req, res){
-    res.json("/members");
-  });
-
-  app.post("/api/signup", function(req, res) {
-    console.log(req.body);
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
-    }).then(function() {
-      res.redirect(307, "/api/login");
-    }).catch(function(err) {
-      console.log(err);
-      res.json(err);
-      // res.status(422).json(err.errors[0].message);
-    });
-  });
-
-  app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
-  });
-
-  app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    }
-    else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
-  });
-
-  app.get("/arena", function(req, res) {
-    db.pokemonstats.findAll({
-      include: [db.images]
-    }).then(function(result){
-      res.render("battle", {pokemon: result} )
-    });
-  });
-  
   app.get('/api/pokemon/image', function(req,res) {
     db.images.findAll({}).then(function(result) {
       res.json(result)
--- server.js
+++ server.js
@@ -17,19 +17,26 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Static directory
+app.use(require('serve-static')(__dirname + '/../../public'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+
+
+
 app.use(express.static("public"));
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+
 
 // Routes
 // =============================================================
-require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
-
+require("./routes/html-routes.js")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({force: false}).then(function() {
